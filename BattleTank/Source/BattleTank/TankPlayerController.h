ac59569e451669d8fd684f1a4b699305b821f659
Oscar Pettersson <oscar_93@live.se>
2018-09-20 22:44:48 +0900

    changed EditAnywhere to EditDefaultsOnly

--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -33,13 +33,13 @@ private:
 	// Return an OUT parameter, true if hit landscape
 	bool GetSightRayHitLocation(FVector& OurHitLocation) const;
 
-	UPROPERTY(EditAnywhere)
+	UPROPERTY(EditDefaultsOnly)
 	float CrosshairXLocation = 0.5;
 	
-	UPROPERTY(EditAnywhere)
+	UPROPERTY(EditDefaultsOnly)
 	float CrosshairYLocation = 0.3333;
 
-	UPROPERTY(EditAnywhere)
+	UPROPERTY(EditDefaultsOnly)
 	float LineTraceRange = 10000000;
 
 	bool GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const;
